ae5d196be317bc87743bfde126d06e586a638ac6
abdulhafiz0007 <mrsteves77@gmail.com>
2025-02-09 16:05:19 +0500

    fix image path

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import arrow_left from '../../assets/left_arrow.png';
-import arrow_right from '../../assets/left_arrow.png';
+import arrow_right from '../../assets/right_arrow.png';
 
 export const Navbar = () => {
 
