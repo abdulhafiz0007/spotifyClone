297f4610cca43e36784f22d44e2820d3e7b60fdb
abdulhafiz0007 <mrsteves77@gmail.com>
2025-02-09 14:24:49 +0500

    fix basename

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import { PlayerContextProvider } from "./context/PlayerContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="spotifyClone">
+  <Router basename="/spotifyClone">
     <PlayerContextProvider>
       <App />
     </PlayerContextProvider>
-  </BrowserRouter>
+  </Router>
 );
